6f5675ccb883d901f63701085e0f3396eb61b542
박진호 <pjho501137@gmail.com>
2025-04-04 11:09:05 +0900

    first commit

---
+++ PlanMate/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PlanMate;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
