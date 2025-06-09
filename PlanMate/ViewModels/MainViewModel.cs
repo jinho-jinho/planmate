ca9903eb54609c46a416e9d3f31edb0fb412dee8
aram0127 <a67891@naver.com>
2025-06-09 23:02:29 +0900

    날씨 기능 - 완 ?

--- PlanMate/ViewModels/MainViewModel.cs
+++ PlanMate/ViewModels/MainViewModel.cs
@@ -10,6 +10,10 @@ using PlanMate.Models;
 using PlanMate.Services;
 using System.Collections.Specialized;
 using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
+using PlanMate.Views;
+using System.Diagnostics;
+using System.Windows.Threading;
 
 namespace PlanMate.ViewModels
 {
@@ -19,8 +23,9 @@ namespace PlanMate.ViewModels
         protected void OnPropertyChanged(string name)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
-        private const string JsonFileName = "schedules.json";
+        public string CurrentDate => DateTime.Now.ToString("yyyy/MM/dd");
 
+        #region 메모
         private const string MemoJsonFileName = "memos.json";
         private readonly JsonStorageService _memoStorageService;
 
@@ -41,13 +46,10 @@ namespace PlanMate.ViewModels
                 CommandManager.InvalidateRequerySuggested();
             }
         }
+        #endregion
 
-        // 커맨드
-        public ICommand AddScheduleCommand { get; }
-        public ICommand AddMemoCommand { get; }
-        public ICommand RemoveMemoCommand { get; }
-
-        public string CurrentDate => DateTime.Now.ToString("yyyy/MM/dd");
+        #region 시간표
+        private const string JsonFileName = "schedules.json";
 
         // 시간축 레이블: 0:00 ~ 23:00
         public ObservableCollection<string> TimeLabels { get; }
@@ -57,6 +59,42 @@ namespace PlanMate.ViewModels
         // 시간표 아이템 컬렉션 추가
         public ObservableCollection<ScheduleItem> ScheduleItems { get; } 
             = new ObservableCollection<ScheduleItem>();
+
+        #endregion
+
+        #region 날씨
+        private string _location;
+        public string Location
+        {
+            get => _location;
+            set { _location = value; OnPropertyChanged(nameof(Location)); }
+        }
+
+        private string _temperature;
+        public string Temperature
+        {
+            get => _temperature;
+            set { _temperature = value; OnPropertyChanged(nameof(Temperature)); }
+        }
+
+        private BitmapImage _weatherIcon;
+        public BitmapImage WeatherIcon
+        {
+            get => _weatherIcon;
+            set { _weatherIcon = value; OnPropertyChanged(nameof(WeatherIcon)); }
+        }
+
+        public ObservableCollection<ForecastItem> Forecasts { get; } = new ObservableCollection<ForecastItem>();
+        public ICommand ShowForecastCommand { get; }
+
+        private readonly WeatherService _weatherService;
+        private readonly DispatcherTimer _timer;
+        #endregion
+
+        // 커맨드
+        public ICommand AddScheduleCommand { get; }
+        public ICommand AddMemoCommand { get; }
+        public ICommand RemoveMemoCommand { get; }
 
         public MainViewModel()
         {
@@ -88,38 +126,20 @@ namespace PlanMate.ViewModels
             AddMemoCommand = new RelayCommand(_ => AddMemo());
             RemoveMemoCommand = new RelayCommand(_ => RemoveMemo(), _ => SelectedMemo != null);
             #endregion
+
+            #region 날씨
+            _weatherService = new WeatherService();
+            ShowForecastCommand = new RelayCommand(_ => LoadAndShowForecast());
+
+            // 초기 날씨 로드 및 10분 간격 갱신
+            LoadWeatherAsync();
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromMinutes(10) };
+            _timer.Tick += async (_, __) => await LoadWeatherAsync();
+            _timer.Start();
+            #endregion
         }
 
-        #region 메모
-        private void MemoCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            if (e.NewItems != null) foreach (MemoItem m in e.NewItems) m.PropertyChanged += MemoPropertyChanged;
-            if (e.OldItems != null) foreach (MemoItem m in e.OldItems) m.PropertyChanged -= MemoPropertyChanged;
-            SaveMemos();
-        }
-
-        private void MemoPropertyChanged(object sender, PropertyChangedEventArgs e) => SaveMemos();
-
-        private void AddMemo()
-        {
-            var memo = new MemoItem { Title = "새 메모", Content = string.Empty };
-            Memos.Add(memo);
-            SelectedMemo = memo;
-        }
-
-        private void RemoveMemo()
-        {
-            if (SelectedMemo == null) return;
-            var toRemove = SelectedMemo;
-            SelectedMemo = Memos.FirstOrDefault(m => m != toRemove);
-            Memos.Remove(toRemove);
-        }
-
-        private void SaveMemos() => _memoStorageService.SaveMemos(Memos);
-        #endregion
-
-        #region JSON 저장/로드
-
+        #region 시간표 기능
         private void LoadFromJson()
         {
             try
@@ -169,10 +189,6 @@ namespace PlanMate.ViewModels
             }
         }
 
-        #endregion
-
-        #region 커맨드 핸들러
-
         private void OnAddSchedule()
         {
             // ScheduleDialog를 추가 모드로 열기
@@ -188,6 +204,70 @@ namespace PlanMate.ViewModels
             }
         }
 
+        #endregion
+
+        #region 메모 기능
+        private void MemoCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null) foreach (MemoItem m in e.NewItems) m.PropertyChanged += MemoPropertyChanged;
+            if (e.OldItems != null) foreach (MemoItem m in e.OldItems) m.PropertyChanged -= MemoPropertyChanged;
+            SaveMemos();
+        }
+
+        private void MemoPropertyChanged(object sender, PropertyChangedEventArgs e) => SaveMemos();
+
+        private void AddMemo()
+        {
+            var memo = new MemoItem { Title = "새 메모", Content = string.Empty };
+            Memos.Add(memo);
+            SelectedMemo = memo;
+        }
+
+        private void RemoveMemo()
+        {
+            if (SelectedMemo == null) return;
+            var toRemove = SelectedMemo;
+            SelectedMemo = Memos.FirstOrDefault(m => m != toRemove);
+            Memos.Remove(toRemove);
+        }
+
+        private void SaveMemos() => _memoStorageService.SaveMemos(Memos);
+        #endregion
+
+        #region 날씨 기능
+        private async Task LoadWeatherAsync()
+        {
+            try
+            {
+                var (city, temp, icon) = await _weatherService.GetCurrentWeatherAsync();
+                Location = city;
+                Temperature = $"{temp:F1}°C";
+                WeatherIcon = new BitmapImage(new Uri($"https://openweathermap.org/img/wn/{icon}@2x.png"));
+                OnPropertyChanged(nameof(CurrentDate));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Weather load failed: {ex.Message}");
+            }
+        }
+
+        private async void LoadAndShowForecast()
+        {
+            try
+            {
+                var list = await _weatherService.Get5DayForecastAsync();
+                Forecasts.Clear();
+                foreach (var item in list) Forecasts.Add(item);
+
+                var window = new PlanMate.Views.ForecastWindow { DataContext = this };
+                window.Owner = App.Current.MainWindow;
+                window.Show();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Forecast load failed: {ex.Message}");
+            }
+        }
         #endregion
     }
 }
